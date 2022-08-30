0c5b3e9d3001203b651e7400eba48e1106cbae05
Renato Lira <62163857+lirarenato22@users.noreply.github.com>
2022-08-29 22:36:13 -0300

    Add files via upload

---
+++ ex_07_estruturas_de_controle.R
@@ -0,0 +1,15 @@
+# EXERCÍCIO - ESTRUTURAS DE CONTROLE
+
+# Aluno: Renato Lira
+
+
+# Exemplo de Controle Condicional
+
+numeros <- runif(1, 67, 108)  
+
+if(numeros > 78) {
+  quantidade <- "muito"
+} else {
+  quantidade <- "pouco"
+}
+quantidade
