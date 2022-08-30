bbeaaa819bfa790c22bd303185645cbbdc93f9d8
Renato Lira <62163857+lirarenato22@users.noreply.github.com>
2022-08-29 22:25:33 -0300

    Add files via upload

---
+++ ex_06_index_operadores_logicos.R
@@ -0,0 +1,20 @@
+# EXERCÍCIO - INDEX E OPERADORES LÓGICOS
+
+# Aluno: Renato Lira
+
+
+# Operadores Lógicos aplicados
+
+
+r <- 89
+
+h <- 67
+
+d <- 45
+
+
+soma_objetos <- c(r, h, d)
+
+soma_objetos <= 879
+
+is.atomic(soma_objetos)
