5d349460befcc9b354f110155f0167af0868c2fb
Renato Lira <62163857+lirarenato22@users.noreply.github.com>
2022-09-17 18:48:09 -0300

    Add files via upload

---
+++ ex_10_etl_na_pratica.R
@@ -0,0 +1,25 @@
+# EXERCÍCIO - ETL na prática
+
+# Aluno: Renato Lira
+
+# carrega a base de snistros de transito do site da PCR
+sinistrosRecife2020Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/fc1c8460-0406-4fff-b51a-e79205d1f1ab/download/acidentes_2020-novo.csv', sep = ';', encoding = 'UTF-8')
+
+sinistrosRecife2020Raw2 <- sinistrosRecife2020Raw[,-c(14)]
+
+sinistrosRecife2021Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2021-jan.csv', sep = ';', encoding = 'UTF-8')
+
+
+sinistrosRecife2019Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2019-jan.csv', sep = ';', encoding = 'UTF-8')
+
+
+sinistrosRecife2018Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2018-jan.csv', sep = ';', encoding = 'UTF-8')
+
+
+sinistrosRecifeRaw <- rbind(sinistrosRecife2020Raw2, sinistrosRecife2021Raw, 
+                            sinistrosRecife2019Raw, sinistrosRecife2018Raw)
+
+
+
+sinistrosRecifeRaw$tipo <- as.factor(sinistrosRecifeRaw$tipo)
+
