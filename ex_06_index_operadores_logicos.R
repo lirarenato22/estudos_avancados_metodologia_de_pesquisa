5d2c85919696f82c0838478eb9bc524641fec220
Renato Lira <62163857+lirarenato22@users.noreply.github.com>
2022-10-27 16:12:41 -0300

    Add files via upload

--- ex_06_index_operadores_logicos.R
+++ ex_06_index_operadores_logicos.R
@@ -1,10 +1,14 @@
-# EXERCÍCIO - INDEX E OPERADORES LÓGICOS
+# EXERCÍCIO - Index e Operadores Lógicos
 
 # Aluno: Renato Lira
 
+# QUESTÃO: Compartilhe com a gente um código criado por você em que você 
+# recupera ou identifica um dado ou informação usando indexação. 
+# Lembre-se de compartilhar um link do github!
 
-# Operadores Lógicos aplicados
+# RESPOSTA:
 
+# Operadores: 
 
 r <- 89
 
@@ -18,3 +22,10 @@ soma_objetos <- c(r, h, d)
 soma_objetos <= 879
 
 is.atomic(soma_objetos)
+
+# Indexação:
+
+USArrests[, 'Assault', drop = FALSE]
+
+USArrests[, 'Rape']
+
