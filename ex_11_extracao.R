5a0444cc8ca234f7c813c9870b2a1b669207ada4
Renato Lira <62163857+lirarenato22@users.noreply.github.com>
2022-10-27 17:12:46 -0300

    Add files via upload

--- ex_11_extracao.R
+++ ex_11_extracao.R
@@ -1,29 +1,55 @@
-# EXERCÍCIO - Extração - Área Intermediária
+# EXERCÍCIO - Extração 
 
 # Aluno: Renato Lira
 
-# carrega a base de snistros de transito do site da PCR
-sinistrosRecife2020Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/fc1c8460-0406-4fff-b51a-e79205d1f1ab/download/acidentes_2020-novo.csv', sep = ';', encoding = 'UTF-8')
+# QUESTÃO: Mostre que você entendeu o conceito de área intermediária e
+# ambiente no R, modificando o código para manter sinistrosRecifeRaw
+# e a função naZero (ela pode ser útil no futuro!). Além disso, 
+# indique qual dos objetos na área intermediária mais estavam usando memória
+# do R. Lembre-se de compartilhar um link do github!
 
-sinistrosRecife2020Raw2 <- sinistrosRecife2020Raw[,-c(14)]
+# RESPOSTA:
+
+# Carregando pacote: 
+
+pacman::p_load(dplyr)
+
+# Abrindo as bases:
+
+sinistrosRecife2019Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/3531bafe-d47d-415e-b154-a881081ac76c/download/acidentes-2019.csv', sep = ';', encoding = 'UTF-8')
+
+# Abrindo as base de sinistros do site PCR: 
+
+sinistrosRecife2020Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/fc1c8460-0406-4fff-b51a-e79205d1f1ab/download/acidentes_2020-novo.csv', sep = ';', encoding = 'UTF-8')
 
 sinistrosRecife2021Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2021-jan.csv', sep = ';', encoding = 'UTF-8')
 
+# Adicionando um ano:
 
-sinistrosRecife2019Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2019-jan.csv', sep = ';', encoding = 'UTF-8')
+sinistrosRecife2018Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2485590a-3b35-4ad0-b955-8dfc36b61021/download/acidentes_2018.csv')
+# Repetindo o comando do docente:
+
+colunas_iguais <- names(sinistrosRecife2020Raw[
+  intersect(
+    names(sinistrosRecife2020Raw), names(sinistrosRecife2021Raw))])
+
+sinistrosRecife2020Raw <- sinistrosRecife2020Raw %>% select(all_of(colunas_iguais))
+
+sinistrosRecifeRaw <- rbind(sinistrosRecife2020Raw, sinistrosRecife2021Raw)
 
 
-sinistrosRecife2018Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2018-jan.csv', sep = ';', encoding = 'UTF-8')
+# Trazendo mais um ano (2019):
+
+colnames(sinistrosRecife2019Raw)[which(names(sinistrosRecife2019Raw) == "data")] <- "dados"
+
+sinistrao_recife <- bind_rows(sinistrosRecife2019Raw, sinistrosRecife2020Raw)
+
+sinistrao_recife$DATA <- as.Date(sinistrao_recife$DATA, format = "%Y-%m-%d")
+
+sinistrao_recife$descricao <- as.factor(sinistrao_recife$descricao)
 
 
-sinistrosRecifeRaw <- rbind(sinistrosRecife2020Raw2, sinistrosRecife2021Raw, 
-                            sinistrosRecife2019Raw, sinistrosRecife2018Raw)
-
-
-
-sinistrosRecifeRaw$tipo <- as.factor(sinistrosRecifeRaw$tipo)
-
-# Para ver quanto cada objeto está ocupando de espaço, utilizar a sequência:
+# Aplicando os princípios de Extração:
 
 for (itm in ls()) { 
   print(formatC(c(itm, object.size(get(itm))), 
@@ -32,17 +58,14 @@ for (itm in ls()) {
         quote=F)
 }
 
+# sinistrao_recife era o que ocupava mais.
 
-# Dos objetos apresentados, o que mais ocupa espaço é "sinistrosRecifeRaw" (3730336).
+# Agora, observando todos:
 
+ls()
 
-# Em sequência,utilizamos a função naZero, de acordo com o solicitado:
+gc()
 
-naZero <- function(x) {
-  x <- ifelse(is.na(x), 0, x)
-}
+# Agora, modificando e removendo:
 
-sinistrosRecifeRaw[, 15:25] <- sapply(sinistrosRecifeRaw[, 15:25], naZero)
-
-
-
+rm(list = ls()[!ls() %in% c("sinistrao_recife", "naZero")])
