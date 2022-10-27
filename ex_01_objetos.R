f71f0ddee3a7f93a42a6293dfe66f45ec13c163c
Renato Lira <62163857+lirarenato22@users.noreply.github.com>
2022-10-27 15:48:56 -0300

    Add files via upload

--- ex_01_objetos.R
+++ ex_01_objetos.R
@@ -1,3 +1,12 @@
+# EXERCÍCIO - Objetos e Funções no R
+
+# Aluno: Renato Lira
+
+# QUESTÃO: Dê um exemplo de código que cria um objeto simples 
+# (como o vetor acima) e que cria um objeto complexo, 
+# criado via uma função (como a regressão acima)
+
+# RESPOSTA:
 
 # Criar objeto simples
 
@@ -5,4 +14,8 @@ vetor <- c(2, 4, 8, 16, 32)
 
 # Criar objeto complexo
 
-teste_6 <- lm(PONTUAÇÃO_GCI ~ PONTUAÇÃO_IDH, data = BANCO_GCI_2018)
+teste_1 <- lm(UrbanPop ~ Assault, data = USArrestsDT)
+
+summary(teste_1)
+
+str(teste_1)
